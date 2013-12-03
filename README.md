331f11ef150ed3abf06d6ee9eb2cdd5644f51823
Ruslan Bredikhin <rus.brn@gmail.com>
2013-12-03 14:11:49 -0500

    Initial version.

---
+++ README.md
@@ -0,0 +1,24 @@
+# An example of Sails.js-project testing with Mocha and Barrels for fixtures
+
+## Installation
+
+  `$ npm install`
+
+## Usage
+
+`npm test`
+
+Use `npm run autotest` for auto-reloading with [Supervisor](https://npmjs.org/package/supervisor).
+
+## Dependencies
+
+* [Sails.js](http://sailsjs.org/)
+* [Mocha](http://visionmedia.github.io/mocha/)
+* [Barrels](https://github.com/bredikhin/barrels/)
+
+
+## License
+
+[The MIT License](http://opensource.org/licenses/MIT)
+
+Copyright (c) 2013 [Ruslan Bredikhin](http://ruslanbredikhin.com/)
---
+++ config/adapters.js
@@ -0,0 +1,29 @@
+/**
+ * Global adapter config
+ * 
+ * The `adapters` configuration object lets you create different global "saved settings"
+ * that you can mix and match in your models.  The `default` option indicates which 
+ * "saved setting" should be used if a model doesn't have an adapter specified.
+ *
+ * Keep in mind that options you define directly in your model definitions
+ * will override these settings.
+ *
+ * For more information on adapter configuration, check out:
+ * http://sailsjs.org/#documentation
+ */
+
+module.exports.adapters = {
+  // If you leave the adapter config unspecified 
+  // in a model definition, 'default' will be used.
+  'default': process.env.NODE_ENV || 'development',
+
+  // Development (disk)
+  development: {
+    module   : 'sails-disk'
+  },
+
+  // Test (memory)
+  test: {
+    module   : 'sails-memory'
+  }
+};
