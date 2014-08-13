7d89cfd11d15b513284c7cc5555a88598aab3298
Ruslan Bredikhin <rus.brn@gmail.com>
2014-08-12 20:41:00 -0400

    generate models and controllers

---
+++ api/controllers/ApplesController.js
@@ -0,0 +1,11 @@
+/**
+ * ApplesController
+ *
+ * @description :: Server-side logic for managing apples
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
